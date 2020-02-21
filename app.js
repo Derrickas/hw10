025273613327117931ed6bb888ce735aa032aeae
Derrickas <Derrickas@outlook.com>
2020-02-20 22:44:16 -0600

    build new repository

---
+++ app.js
@@ -0,0 +1,4 @@
+const inquirer = require('./inquirer')
+const manager = require('./templates/manager')
+const engineer = require('./templates/engineer')
+const intern = require('./templates/intern')
